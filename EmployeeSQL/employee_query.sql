23f6026d1c2ffbfde6ddc9156f2d598ed77244b7
emichellet <emichelletan@gmail.com>
2023-03-03 05:09:38 -0500

    initial commit

--- EmployeeSQL/employee_query.sql
+++ EmployeeSQL/employee_query.sql
@@ -1,110 +1,56 @@
+-- Create the table schemas
+
+-- Create "titles" table
 CREATE TABLE titles(
-		title_id VARCHAR PRIMARY KEY,
-		title VARCHAR NOT NULL
+		"title_id" VARCHAR(5) NOT NULL,
+		"title" VARCHAR(30) NOT NULL,
+		CONSTRAINT "pk_titles" PRIMARY KEY (
+		"title_id"
+		)
 );
 
-
+-- Create departments table
 CREATE TABLE departments(
-		dept_no VARCHAR PRIMARY KEY,
-		dept_name VARCHAR
+		"dept_no" VARCHAR(5) PRIMARY KEY,
+		"dept_name" VARCHAR(10)
 );
 
-
+-- Create employees table
 CREATE TABLE employees(
-		emp_no INT PRIMARY KEY,
-		emp_title_id VARCHAR NOT NULL,
-		birth_date VARCHAR NOT NULL,
-		first_name VARCHAR NOT NULL,
-		last_name VARCHAR NOT NULL,
-		sex VARCHAR NOT NULL,
-		hire_date VARCHAR NOT NULL,
+		emp_no VARCHAR(10) PRIMARY KEY,
+		emp_title_id VARCHAR(5) NOT NULL,
+		birth_date date,
+		first_name VARCHAR(45) NOT NULL,
+		last_name VARCHAR(45) NOT NULL,
+		sex VARCHAR(1),
+		hire_date date,
 		FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
-
--- Because the dept_emp table is a many-many relationship, two primary keys are needed
+-- Create department empoyees table
+-- Note: Because the dept_emp table is a many-many relationship, two primary keys are needed
 CREATE TABLE dept_emp(
-		emp_no INT NOT NULL,
-		dept_no VARCHAR NOT NULL,
+		emp_no VARCHAR(10) references employees(emp_no),
+		dept_no VARCHAR(5),
 		PRIMARY KEY (emp_no, dept_no),
 		FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 		FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
-
+-- Create salaries table
 CREATE TABLE salaries(
-		emp_no INT PRIMARY KEY,
-		salary INT NOT NULL,
+		emp_no VARCHAR(10) REFERENCES employees(emp_no),
+		salary INT,
 		FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 
-
+-- Create a department managers table
 -- The dept_manager table is also a many-many relationship, therefore, two primary keys are required again
 CREATE TABLE dept_manager(
-		dept_no VARCHAR NOT NULL,
-		emp_no INT NOT NULL,
+		dept_no VARCHAR(5) references departments(dept_no),
+		emp_no VARCHAR(10),
 		PRIMARY KEY(dept_no, emp_no),
 		FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
 		FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
-
-
--- DATA ANALYSIS
--- 1. List the following details of each employee: employee number, last name, first name, sex, and salary of each employee.
-SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
-FROM employees
-JOIN salaries ON employees.emp_no = salaries.emp_no;
-
-
--- 2. List the first name, last name, and hire date for the employees who were hired in 1986.
-SELECT first_name, last_name, hire_date
-FROM employees
-WHERE hire_date LIKE '%1986';
-
-
--- 3. List the manager of each department along with their department number, department name, employee number, last name, and first name.
-SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
-FROM dept_manager
-JOIN departments ON dept_manager.dept_no = departments.dept_no
-JOIN employees ON dept_manager.emp_no = employees.emp_no;
-
-
--- 4. List the department number for each employee along with that employee's employee number, last name, first name, and department name.
-SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
-FROM employees
-JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
-JOIN departments ON dept_emp.dept_no = departments.dept_no;
-
-
--- 5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
-SELECT first_name, last_name, sex
--- Department number of Sales is d007
-FROM employees
-WHERE first_name = 'Hercules' and last_name LIKE 'B%';
-
-
--- 6. List each employee in the Sales department, including their employee number, last name, and first name.
-SELECT * FROM departments;
-SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
-FROM employees
-JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
-JOIN departments ON dept_emp.dept_no = departments.dept_no
-WHERE departments.dept_no = 'd007';
-
-
--- 7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
-SELECT * FROM departments;
--- Department numbers of Sales and Development are d007 and d005
-SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
-FROM employees
-JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
-JOIN departments ON dept_emp.dept_no = departments.dept_no
-WHERE departments.dept_no = 'd005' or departments.dept_no = 'd007';
-
-
--- 8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
-SELECT last_name, COUNT(last_name) AS "Frequency_count"
-FROM employees
-GROUP BY last_name
-ORDER BY "Frequency_count" DESC;
